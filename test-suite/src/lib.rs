5e4620033b9ce3d0b25ffb99da4e15e6024b8efd
Hanno Braun <hanno@braun-embedded.com>
2020-01-28 12:50:52 +0100

    Move `TestStand` to dedicated module

--- test-suite/src/lib.rs
+++ test-suite/src/lib.rs
@@ -9,55 +9,13 @@ pub mod config;
 pub mod result;
 pub mod serial;
 pub mod target;
+pub mod test_stand;
 
 
-pub use self::result::{
-    Result,
-    Error,
+pub use self::{
+    result::{
+        Result,
+        Error,
+    },
+    test_stand::TestStand,
 };
-
-
-use self::{
-    config::Config,
-    serial::Serial,
-    target::Target,
-};
-
-
-/// An instance of the test stand
-///
-/// Used to access all resources that a test case requires.
-pub struct TestStand {
-    target: Target,
-    serial: Serial,
-}
-
-impl TestStand {
-    /// Initializes the test stand
-    ///
-    /// Reads the `test-stand.toml` configuration file and initializes test
-    /// stand resources, as configured in there.
-    pub fn new() -> Result<Self> {
-        let config = Config::read()?;
-
-        let target = Target::new(&config.target)?;
-        let serial = Serial::new(&config.serial)?;
-
-        Ok(
-            TestStand {
-                target,
-                serial,
-            }
-        )
-    }
-
-    /// Returns the connection to the test target (device under test)
-    pub fn target(&mut self) -> &mut Target {
-        &mut self.target
-    }
-
-    /// Returns the connection to the Serial-to-USB converter
-    pub fn serial(&mut self) -> &mut Serial {
-        &mut self.serial
-    }
-}
