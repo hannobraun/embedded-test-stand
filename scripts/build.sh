e33aea7814148d7c4d3a5f65f919683c92e649fe
Hanno Braun <hanno@braun-embedded.com>
2020-02-04 13:02:53 +0100

    Execute test-lib tests in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,14 @@ set -e
 export RUSTFLAGS="-D warnings"
 
 (
+    cd test-lib
+    cargo test --verbose --features=firmware)
+(
+    cd test-lib
+    cargo test --verbose --features=host)
+(
     cd test-firmware
     cargo build --verbose)
-
 (
     cd test-suite
     cargo build --tests --verbose)
