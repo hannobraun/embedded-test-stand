a18603e25e64855abfc458c1f78e45ced15d7dc0
Hanno Braun <hanno@braun-embedded.com>
2020-03-03 13:13:43 +0100

    Add messages for host-assistant communication
    These currently mirror the message types used for host-target
    communication, but I'm sure differences will emerge over time.

--- messages/src/lib.rs
+++ messages/src/lib.rs
@@ -14,10 +14,24 @@ pub enum HostToTarget<'r> {
     SendUsart(&'r [u8]),
 }
 
-
 /// An message from the target to the test suite on the host
 #[derive(Deserialize, Serialize)]
 pub enum TargetToHost<'r> {
     /// Notify the host that data has been received via USART
+    UsartReceive(&'r [u8]),
+}
+
+
+/// A message from the test suite on the host to the test assistant
+#[derive(Deserialize, Serialize)]
+pub enum HostToAssistant<'r> {
+    /// Instruct the assistant to send data to the target via USART
+    SendUsart(&'r [u8]),
+}
+
+/// A message from the test assistant to the test suite on the host
+#[derive(Deserialize, Serialize)]
+pub enum AssistantToHost<'r> {
+    /// Notify the host that data has been received from the target via USART
     UsartReceive(&'r [u8]),
 }
