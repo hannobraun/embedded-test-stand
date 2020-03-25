1a68175eaca267e813a7109705a05967cc14af9a
Hanno Braun <hanno@braun-embedded.com>
2020-03-25 18:45:19 +0100

    Make target and assistant required in suite
    They are used by all the test cases, so it makes sense to ease their use
    and front-load the potential configuration error.

--- test-suite/src/test_stand.rs
+++ test-suite/src/test_stand.rs
@@ -21,8 +21,8 @@ use super::{
 pub struct TestStand {
     _guard: LockResult<MutexGuard<'static, ()>>,
 
-    pub target:    Result<Conn, NotConfiguredError>,
-    pub assistant: Result<Conn, NotConfiguredError>,
+    pub target:    Conn,
+    pub assistant: Conn,
     pub serial:    Result<Serial, NotConfiguredError>,
 }
 
@@ -38,25 +38,21 @@ impl TestStand {
         Ok(
             Self {
                 _guard:    test_stand.guard,
-                target:    test_stand.target,
-                assistant: test_stand.assistant,
+                target:    test_stand.target?,
+                assistant: test_stand.assistant?,
                 serial:    test_stand.serial,
             }
         )
     }
 
     /// Returns the connection to the test target (device under test)
-    pub fn target(&mut self) -> Result<Target, NotConfiguredError> {
-        self.target.as_mut()
-            .map(|target| Target(target))
-            .map_err(|err| *err)
+    pub fn target(&mut self) -> Target {
+        Target(&mut self.target)
     }
 
     /// Returns the connection to the test assistant
-    pub fn assistant(&mut self) -> Result<Assistant, NotConfiguredError> {
-        self.assistant.as_mut()
-            .map(|assistant| Assistant(assistant))
-            .map_err(|err| *err)
+    pub fn assistant(&mut self) -> Assistant {
+        Assistant(&mut self.assistant)
     }
 
     /// Returns the connection to the Serial-to-USB converter
@@ -70,4 +66,11 @@ impl TestStand {
 #[derive(Debug)]
 pub enum TestStandInitError {
     Inner(host_lib::test_stand::TestStandInitError),
+    NotConfigured(NotConfiguredError),
+}
+
+impl From<NotConfiguredError> for TestStandInitError {
+    fn from(err: NotConfiguredError) -> Self {
+        Self::NotConfigured(err)
+    }
 }
